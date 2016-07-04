3dbd90c1cbc26ee2054470a8f98604368eecae19
Dima Kanarski <belython@gmail.com>
2016-07-04 20:02:57 +0300

    Fourth change

--- src/by/booking/entities/Order.java
+++ src/by/booking/entities/Order.java
@@ -6,6 +6,38 @@ public class Order{
     private User user;
     private int totalPersons;
     private RoomType roomType;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        if (id != order.id) return false;
+        if (totalPersons != order.totalPersons) return false;
+        if (checkInDate != order.checkInDate) return false;
+        if (checkOutDate != order.checkOutDate) return false;
+        if (!user.equals(order.user)) return false;
+        if (!roomType.equals(order.roomType)) return false;
+        if (!hotel.equals(order.hotel)) return false;
+        return status.equals(order.status);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + user.hashCode();
+        result = 31 * result + totalPersons;
+        result = 31 * result + roomType.hashCode();
+        result = 31 * result + hotel.hashCode();
+        result = 31 * result + (int) (checkInDate ^ (checkInDate >>> 32));
+        result = 31 * result + (int) (checkOutDate ^ (checkOutDate >>> 32));
+        result = 31 * result + status.hashCode();
+        return result;
+    }
+
     private Hotel hotel;
     private long checkInDate;
     private long checkOutDate;
